8a28185922ae05e0c106d2093545ba76068f4dd7
Kostis Litsios <kostislitsios@gmail.com>
2024-12-09 18:11:00 +0200

    workout schedule tests

--- service/DefaultService.js
+++ service/DefaultService.js
@@ -232,13 +232,7 @@ exports.getAllExercises = function(searchText,filter) {
  **/
 exports.getWorkoutSchedule = function(userID) {
   return new Promise(function(resolve, reject) {
-    var examples = {};
-    examples['application/json'] = [ "", "" ];
-    if (Object.keys(examples).length > 0) {
-      resolve(examples[Object.keys(examples)[0]]);
-    } else {
-      resolve();
-    }
+    resolve();
   });
 }
 
